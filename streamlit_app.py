f9b55b4cd306d803ba3a8bd2d94c8ce4f9cd533b
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-09-08 11:30:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ from sendgrid.helpers.mail import Mail
 import stripe
 
 
-st.set_page_config("Pickiepoint", "pickiepoint_logo_profile_picture_96x96.png")
+st.set_page_config("Pickiepoint", "pickiepoint_logo_profile_picture_192x192.png")
 
 # DELETE THIS AFTER TEST
 hide_st_style = """
